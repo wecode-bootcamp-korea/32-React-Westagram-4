34e3afdb7f60f45f51277cca91434b400eeadee9
rkekqmf <rkekqmf@gmail.com>
2022-04-13 19:59:29 +0900

    test

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -1,26 +1,32 @@
-import React from "react";
+import React from 'react';
 import './Nav.scss';
 
 const Nav = () => {
   return (
-    
-    <div className='main-container'>
-    <nav className='nav-container'>
-        <div className='nav-left'>
-          <img className='nav-icon' alt='zz' src={'images/navImage/instagram_icon.png'} />
-          <span className='nav-title'>&#x0007C; Westagram</span>
+    <div className="main-container">
+      <nav className="nav-container">
+        <div className="nav-left">
+          <img
+            className="nav-icon"
+            alt="zz"
+            src="images/navImage/instagram_icon.png"
+          />
+          <span className="nav-title">&#x0007C; Westagram</span>
         </div>
-        <div className='nav-center'>
-          <input className='nav-search' type='text' placeholder='검색' />
+        <div className="nav-center">
+          <input className="nav-search" type="text" placeholder="검색" />
         </div>
-        <div className='nav-right'>
-          <img className='nav-icon' src={'images/navImage/explore_icon.png'} />
-          <img className='nav-icon' src={'images/navImage/heart_empty_icon.png'} />
-          <img className='nav-icon' src={'images/navImage/profile_icon.png'} />
+        <div className="nav-right">
+          <img className="nav-icon" src="images/navImage/explore_icon.png" />
+          <img
+            className="nav-icon"
+            src="images/navImage/heart_empty_icon.png"
+          />
+          <img className="nav-icon" src="images/navImage/profile_icon.png" />
         </div>
       </nav>
-      </div>
-);
-}
+    </div>
+  );
+};
 
 export default Nav;
