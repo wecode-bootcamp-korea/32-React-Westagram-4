f159e404c5392a10a4a078cca2d00d38a3cc3d32
Lily <didwodnjs7268@gmail.com>
2022-04-18 18:55:52 +0900

    Add: 로그인, 메인 페이지 scss 작업 완료

--- src/pages/jaewonyang/Main/Aside/AsideJaeWon.js
+++ src/pages/jaewonyang/Main/Aside/AsideJaeWon.js
@@ -1,7 +1,127 @@
-import React from "react";
+import React from 'react';
+import './AsideJaeWon.scss';
 
-function Aside() {
-  return <div>Aside</div>;
+function AsideJaeWon() {
+  return (
+    <div className="AsideJaeWon">
+      <div className="main__right__header">
+        <img
+          alt="profile image"
+          src="../images/jaewonyang/profile.jpg"
+          className="header__profile"
+        />
+        <div className="header__account">
+          <p className="account">wecode_bootcamp</p>
+          <p className="description">WeCode | 위코드</p>
+        </div>
+      </div>
+      <div className="main__right__articles">
+        <div className="article article__story">
+          <div className="article__title">
+            <span className="story">스토리</span>
+            <span className="all">모두 보기</span>
+          </div>
+          <div className="article__items">
+            <div className="items__item">
+              <img
+                alt="profile image"
+                src="../images/jaewonyang/profile.jpg"
+                className="item__profile"
+              />
+              <div className="item__account">
+                <div className="account">_yum_S</div>
+                <div className="info">16분 전</div>
+              </div>
+            </div>
+            <div className="items__item">
+              <img
+                alt="profile image"
+                src="../images/jaewonyang/profile.jpg"
+                className="item__profile"
+              />
+              <div className="item__account">
+                <div className="account">drink_eat_drink</div>
+                <div className="info">3시간 전</div>
+              </div>
+            </div>
+            <div className="items__item">
+              <img
+                alt="profile image"
+                src="../images/jaewonyang/profile.jpg"
+                className="item__profile"
+              />
+              <div className="item__account">
+                <div className="account">hyukyc</div>
+                <div className="info">20시간 전</div>
+              </div>
+            </div>
+            <div className="items__item">
+              <img
+                alt="profile image"
+                src="../images/jaewonyang/profile.jpg"
+                className="item__profile"
+              />
+              <div className="item__account">
+                <div className="account">lalala</div>
+                <div className="info">21시간 전</div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="article article__member ">
+          <div className="article__title">
+            <span className="story">회원님을 위한 추천</span>
+            <span className="all">모두 보기</span>
+          </div>
+          <div className="article__items">
+            <div className="items__item">
+              <img
+                alt="profile image"
+                src="../images/jaewonyang/profile.jpg"
+                className="item__profile"
+              />
+              <div className="item__account">
+                <div className="account">joaaaahye</div>
+                <div className="info">nuganuga님 외 2명이...</div>
+              </div>
+              <div className="item__followBtn">팔로우</div>
+            </div>
+            <div className="items__item">
+              <img
+                alt="profile image"
+                src="../images/jaewonyang/profile.jpg"
+                className="item__profile"
+              />
+              <div className="item__account">
+                <div className="account">ramper21</div>
+                <div className="info">2ourcodffe님 외 12명...</div>
+              </div>
+              <div className="item__followBtn">팔로우</div>
+            </div>
+            <div className="items__item">
+              <img
+                alt="profile image"
+                src="../images/jaewonyang/profile.jpg"
+                className="item__profile"
+              />
+              <div className="item__account">
+                <div className="account">helloWorld</div>
+                <div className="info">jjdsadsfdsfs님 외 3명...</div>
+              </div>
+              <div className="item__followBtn">팔로우</div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="main__right__footer">
+        <p>
+          Instagram 정보·지원·홍보 센터·API·채용
+          정보·개인정보처리방침·약관·디렉터리·프로필·해시태그·언어
+        </p>
+        <p> © 2019 INSTAGRAM</p>
+      </div>
+    </div>
+  );
 }
 
-export default Aside;
+export default AsideJaeWon;
--- src/pages/jaewonyang/Main/MainJaeWon.js
+++ src/pages/jaewonyang/Main/MainJaeWon.js
@@ -1,8 +1,85 @@
 import React from 'react';
+import './MainJaeWon.scss';
+import AsideJaeWon from './Aside/AsideJaeWon';
 
-function Main() {
-  return <div>Main</div>;
-  <Nav />;
+function MainJaeWon() {
+  return (
+    <body className="body">
+      <div className="Main">
+        <div className="MainJaeWon">
+          <header class="main__left__header">
+            <img
+              alt="profile"
+              class="header__profile"
+              src="../images/jaewonyang/profile.jpg"
+            />
+            <span class="header__account">happy_lily</span>
+            <i class="fa-solid fa-ellipsis"></i>
+          </header>
+          <article class="main__left__article">
+            <img
+              alt="post image"
+              src="../images/jaewonyang/main.jpeg"
+              class="article__image"
+            />
+            <div class="article__icons">
+              <button>
+                <i class="fa-solid fa-heart"></i>
+              </button>
+              <button>
+                <i class="fa-solid fa-comment"></i>
+              </button>
+              <button>
+                <i class="fa-solid fa-arrow-up-from-bracket"></i>
+              </button>
+              <button class="bookmark">
+                <i class="fa-solid fa-bookmark"></i>
+              </button>
+            </div>
+            <div class="article__like">
+              <img
+                alt="progile image"
+                src="../images/jaewonyang/profile.jpg"
+                class="like__profile"
+              />
+              <span class="like__account">
+                <span>wecode_official</span>님 <span>외 10명</span>이 좋아합니다
+              </span>
+            </div>
+            <div class="article__content">
+              <span class="content__account">happy_lily</span>
+              <span class="content__text">
+                위워크에서 진행한 베이킹 클래스... <span>더 보기</span>
+              </span>
+            </div>
+          </article>
+          <footer class="main__left__footer">
+            <ul class="footer__comment__results">
+              <li class="footer__comment__result">
+                <span class="comment__account">wecode_official</span>
+                <span class="comment__text">거봐 좋았잖아~~~🌼</span>
+                <button class="comment__heart">
+                  <img src="https://s3.ap-northeast-2.amazonaws.com/cdn.wecode.co.kr/bearu/heart.png" />
+                </button>
+              </li>
+            </ul>
+            <span class="footer__time">42분 전</span>
+            <form class="footer__comment__ipnut">
+              <input
+                type="text"
+                placeholder="댓글 달기..."
+                class="comment__input"
+              />
+              <button class="comment__button" disabled>
+                게시
+              </button>
+            </form>
+          </footer>
+        </div>
+        <AsideJaeWon />
+      </div>
+    </body>
+  );
 }
 
-export default Main;
+export default MainJaeWon;
